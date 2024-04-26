a3ad3313f25d5b173ba73917c4f28b050a45a1ab
mikeitrain1 <142508264+mikeitrain1@users.noreply.github.com>
2024-04-26 15:16:08 +0300

    indent

--- lua/options.lua
+++ lua/options.lua
@@ -35,9 +35,9 @@ vim.opt.autoread = true
 
 -- use spaces for tabs and whatnot
 
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.termguicolors = true
