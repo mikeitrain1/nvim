dabd0ed71badb76ba408295e51bbae6d5eb20a1e
mikeitrain1 <142508264+mikeitrain1@users.noreply.github.com>
2024-04-27 23:57:09 +0300

    harpoon go brrr

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -1,164 +1,164 @@
 return {
 
-    {
-        "lewis6991/gitsigns.nvim",
-        config = function()
-            require('gitsigns').setup()
-        end
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+      require('gitsigns').setup()
+    end
+  },
+
+  {'mfussenegger/nvim-jdtls'},
+
+  {
+    'akinsho/toggleterm.nvim',
+    config = function()
+      require"toggleterm".setup {
+        size = 20,
+        open_mapping = [[<A-1>]],
+        shade_filetypes = {},
+        shade_terminals = true,
+        shading_factor = '1',
+        start_in_insert = true,
+        persist_size = true,
+        direction = 'horizontal'
+      }
+    end
+  },
+
+
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    config = function()
+      require("ibl").setup()
+    end
+  },
+  --
+  -- {
+  --     "nvim-tree/nvim-tree.lua",
+  --     config = function()
+  --         require("nvim-tree").setup()
+  --         vim.keymap.set('n', '<leader>z', ':NvimTreeFocus<CR>')
+  --         vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
+  --     end
+  -- },
+
+
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function ()
+      local harpoon = require("harpoon")
+
+      -- REQUIRED
+      harpoon:setup()
+      -- REQUIRED
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+      vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+      vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+    end
+  },
+
+
+
+
+
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    opts = {} -- this is equalent to setup({}) function
+  },
+
+
+  { 'numToStr/Comment.nvim', opts = {} },
+
+  {
+    "numToStr/Comment.nvim",
+    config = function()
+      require("Comment").setup()
+    end
+  },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000,
+    config = function()
+      require('gruvbox').setup({
+        disable_background = true
+      })
+
+      function ColorMyPencils(color)
+        color = color or "gruvbox"
+        vim.cmd.colorscheme(color)
+
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+      end
+
+      ColorMyPencils()
+
+    end
+  },
+  --
+
+  "williamboman/mason.nvim",
+  "williamboman/mason-lspconfig.nvim",
+  "neovim/nvim-lspconfig",
+
+  'folke/neodev.nvim', -- new
+
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'L3MON4D3/LuaSnip',
+      'saadparwaiz1/cmp_luasnip',
+      'rafamadriz/friendly-snippets',
+
+      'hrsh7th/cmp-nvim-lsp',
+
     },
+  },
 
-    {'mfussenegger/nvim-jdtls'},
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+  },
 
-    {
-        'akinsho/toggleterm.nvim',
-        config = function()
-            require"toggleterm".setup {
-                size = 20,
-                open_mapping = [[<A-1>]],
-                shade_filetypes = {},
-                shade_terminals = true,
-                shading_factor = '1',
-                start_in_insert = true,
-                persist_size = true,
-                direction = 'horizontal'
-            }
-        end
+
+  {
+    'nvim-telescope/telescope.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+
+  {
+    'nvim-telescope/telescope-fzf-native.nvim',
+    build = 'make',
+  },
+
+
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
     },
-
-
-
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        config = function()
-            require("ibl").setup()
-        end
-    },
-    --
-    -- {
-    --     "nvim-tree/nvim-tree.lua",
-    --     config = function()
-    --         require("nvim-tree").setup()
-    --         vim.keymap.set('n', '<leader>z', ':NvimTreeFocus<CR>')
-    --         vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
-    --     end
-    -- },
-
-
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function ()
-            local harpoon = require("harpoon")
-
-            -- REQUIRED
-            harpoon:setup()
-            -- REQUIRED
-
-            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-
-            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
-
-            -- Toggle previous & next buffers stored within Harpoon list
-            vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-            vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
-
-        end
-    },
-
-
-
-
-
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        opts = {} -- this is equalent to setup({}) function
-    },
-
-
-    { 'numToStr/Comment.nvim', opts = {} },
-
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end
-    },
-
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            require('gruvbox').setup({
-                disable_background = true
-            })
-
-            function ColorMyPencils(color)
-                color = color or "gruvbox"
-                vim.cmd.colorscheme(color)
-
-                vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-                vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
-            end
-
-            ColorMyPencils()
-
-        end
-    },
-    --
-
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "neovim/nvim-lspconfig",
-
-    'folke/neodev.nvim', -- new
-
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
-
-            'hrsh7th/cmp-nvim-lsp',
-
-        },
-    },
-
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate',
-    },
-
-
-    {
-        'nvim-telescope/telescope.nvim',
-        dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-
-    {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-    },
-
-
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = {
-            "nvim-tree/nvim-web-devicons"
-        },
-        config = function()
-            require("lualine").setup({
-                icons_enabled = true,
-                theme = 'onedark',
-            })
-        end,
-    }
+    config = function()
+      require("lualine").setup({
+        icons_enabled = true,
+        theme = 'onedark',
+      })
+    end,
+  }
 
 }
