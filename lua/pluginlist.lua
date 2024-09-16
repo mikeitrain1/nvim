d5377437f2a0b28043bd71f5fe822760aaa732ca
mikeitrain1 <142508264+mikeitrain1@users.noreply.github.com>
2024-09-16 17:19:28 +0300

    markdown

--- lua/pluginlist.lua
+++ lua/pluginlist.lua
@@ -1,6 +1,23 @@
 return {
 
-    { "ixru/nvim-markdown" },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+
+
+    {
+        'MeanderingProgrammer/render-markdown.nvim',
+        main = "render-markdown",
+        opts = {},
+        name = 'render-markdown',
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    },
+
     {'mfussenegger/nvim-jdtls'},
 
 
