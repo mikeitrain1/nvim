b4ea77c5df110ec1697ab4e5c600eed1e0c022ec
mikeitrain1 <142508264+mikeitrain1@users.noreply.github.com>
2025-08-13 01:06:48 +0300

    testing

--- init.lua
+++ init.lua
@@ -1,7 +1,10 @@
+require "options"
+require "plugins"
+require "keymaps"
+
 local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
 if not vim.loop.fs_stat(pipepath) then
-  vim.fn.serverstart(pipepath)
+vim.fn.serverstart(pipepath)
 end
-require "options"
-require "keymaps"
-require "plugins"
+
+
